eabefa1ca187fda898841c8bf5ea738c43b9230e
williamedge <williamedge88@gmail.com>
2024-02-01 16:20:42 +0800

    Initial commit for pypi placeholder

---
+++ src/oceanea/__init__.py
@@ -0,0 +1,9 @@
+"""
+oceanea.
+
+Empirical functions for the exploratory analysis of physical oceanography data.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'William Edge'
+__credits__ = 'Initiated from mrayson\'s oceanoi package.'
